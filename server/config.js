d34b7141adce71b4fe7676816b82ae6132457565
ChengShi <sc971008@gmail.com>
2025-11-21 22:25:08 -0500

    update server docker

--- server/config.js
+++ server/config.js
@@ -1,11 +1,13 @@
 // Add configuration setting for your server to this file
 
 // uncomment for MONGO_URL for docker
-const MONGO_URL = process.env.MONGO_URL
 // const MONGO_URL = "mongodb://mongodb:27017/fake_so";
 
 // config for local testing, that is, without docker.
 //const MONGO_URL = "mongodb://localhost:27017/fake_so";
+
+// config for deployment
+const MONGO_URL = process.env.MONGO_URL
 
 const CLIENT_URL = "https://geekdockclient-production.up.railway.app";
 const port = process.env.PORT || 8000;
